1653bd259b314938aa3f5096426d7da2abf5db02
ShindeVedant12 <161154285+ShindeVedant12@users.noreply.github.com>
2024-03-13 12:43:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-import { getDatabase ,set,ref} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+import { getDatabase,set,ref } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,11 +30,12 @@ document.getElementById("register").addEventListener('click',(e) =>{
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
-    alert('user created')
-    const user = userCredential.user;
     set(ref(database, 'users/' + user.uid), {
       username: username,
       email: email,
+
+    });
+    alert('user created')
     // ...
   })
   .catch((error) => {
@@ -64,8 +65,8 @@ document.getElementById("login").addEventListener('click',(e) =>{
     const errorCode = error.code;
     const errorMessage = error.message;
     alert(errorMessage);
-    // ..
-  });
+    // ..
+  });
 })
 
 
